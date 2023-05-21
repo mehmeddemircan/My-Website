68ed6af7a40aa0bf0d2d0942a77f066115212ae1
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 21:34:33 +0300

    created project description feature

---
+++ frontend/src/components/sections/Project/ProjectDescriptionFeature.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react'
+
+const ProjectDescriptionFeature = () => {
+  return (
+   <Fragment>
+     <div className="border-b border-gray-200 pb-6">
+              <p className="text-sm leading-none text-gray-600">
+                Balenciaga Fall Collection
+              </p>
+              <h1
+                className="
+							lg:text-2xl
+							text-xl
+							font-semibold
+							lg:leading-6
+							leading-7
+							text-gray-800
+							mt-2
+						"
+              >
+                Balenciaga Signature Sweatshirt
+              </h1>
+              <div className='mt-4'>
+                <ul className='ms-4 list-disc text-sm text-gray-500 dark:text-gray-400 '>
+                    <li className='my-2'>dassdadsadsa</li>
+                    <li className='my-2'>dassdadsadsa</li>
+                    <li className='my-2'>dassdadsadsa</li>
+                    <li className='my-2'>dassdadsadsa</li>
+                    <li className='my-2'>dassdadsadsa</li>
+                  
+                </ul>
+            </div>
+            </div>
+           
+   </Fragment>
+  )
+}
+
+export default ProjectDescriptionFeature
