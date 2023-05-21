aa40244d7d6a81a8f6db192b67ec70341b19be45
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 22:53:23 +0300

    changed project card url to route

--- frontend/src/components/cards/ProjectCard.js
+++ frontend/src/components/cards/ProjectCard.js
@@ -1,19 +1,19 @@
 import React, { Fragment } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
 
-const ProjectCard = () => {
+const ProjectCard = ({project}) => {
 
   const navigate = useNavigate()
   return (
     <Fragment>
-    <div onClick={() => navigate('/project-details')} class="my-3 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row hover:shadow-lg hover:translate-y-[-4px]">
+    <div onClick={() => navigate(`/projects/${project._id}/details`)} class="my-3 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row hover:shadow-lg hover:translate-y-[-4px]">
   <img
     class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
     src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
     alt=""
   />
   <div class="flex flex-col justify-start p-6">
-    <h5 class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">Card title</h5>
+    <h5 class="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">{project.title}</h5>
     <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
       This is a wider card with supporting text below as a natural lead-in
       to additional content. This content is a little bit longer.
