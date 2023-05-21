ef2e0f3c7acda53abe041dc2a4673c5988912642
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 16:59:20 +0300

    added comment sections and post comment button

--- frontend/src/pages/ProjectDetailsPage.js
+++ frontend/src/pages/ProjectDetailsPage.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import MainLayout from "../layouts/MainLayout";
 import { Image, Space, Tag } from "antd";
+import CommentSections from "../components/sections/CommentSections";
+import CommentModal from "../components/modal/CommentModal";
 
 const ProjectDetailsPage = () => {
   const [show, setShow] = useState(false);
   const [show2, setShow2] = useState(false);
+
+  const [showCommentModal, setShowCommentModal] = useState(false)
+
+  const handleShowCommentModal = () => {
+setShowCommentModal(true)
+  }
+  const handleCloseCommentModal = () => {
+    setShowCommentModal(false)
+  }
+
   return (
     <MainLayout>
       <div className="container mx-auto">
@@ -244,7 +256,21 @@ const ProjectDetailsPage = () => {
             </div>
           </div>
         </div>
+        <div class="inline-flex items-center justify-center w-full">
+    <hr class="w-100 h-px my-8 bg-gray-400 border-0 dark:bg-gray-700" />
+    <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Yorumlar</span>
+</div>
+        <div className="d-flex flex-row justify-end my-3">
+      
+       <button className="btn btn-dark rounded-pill" onClick={handleShowCommentModal}>Yorum Yap</button>
+                <CommentModal 
+                  showCommentModal={showCommentModal}
+                  handleCloseCommentModal={handleCloseCommentModal}
+                />
       </div>
+      <CommentSections />
+      </div>
+     
     </MainLayout>
   );
 };
