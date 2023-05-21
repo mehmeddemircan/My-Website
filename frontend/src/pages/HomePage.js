3412ead17848e947bbdd2f226fb32d8f074b8ac7
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 22:55:06 +0300

    useEffect get projects with pagination

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,22 +1,41 @@
-import React from 'react'
-import MainLayout from '../layouts/MainLayout'
-import HeaderSections from '../components/sections/HeaderSections'
-import ProjectList from '../components/list/ProjectList'
-import ProfileSections from '../components/sections/ProfileSections'
-import ProjectPagination from '../components/pagination/ProjectPagination'
-import MetaTitle from '../meta/MetaTitle'
+import React, { useEffect, useState } from "react";
+import MainLayout from "../layouts/MainLayout";
+import HeaderSections from "../components/sections/HeaderSections";
+import ProjectList from "../components/list/ProjectList";
+import ProfileSections from "../components/sections/ProfileSections";
+import ProjectPagination from "../components/pagination/ProjectPagination";
+import MetaTitle from "../meta/MetaTitle";
+import { useDispatch, useSelector } from "react-redux";
+import { GetProjects } from "../redux/actions/ProjectActions";
 
 const HomePage = () => {
+  const getAllProject = useSelector((state) => state.project.getAllProject);
+  const [limit, setLimit] = useState(4);
+  const [currentPage, setCurrentPage] = useState(1);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(GetProjects(limit, currentPage));
+  }, [dispatch, limit, currentPage]);
+
   return (
-   <MainLayout>
+    <MainLayout>
       <MetaTitle title="Anasayfa" name="anasayfa" content="anasayfa" />
-    <HeaderSections />
+      <HeaderSections />
 
-        <ProjectList />
- 
+      <ProjectList />
+      <ProjectPagination
+        onChange={(page) => setCurrentPage(page)}
+        current={currentPage}
+        defaultCurrent={1}
+        pageSize={limit}
+        total={getAllProject.totalProjects}
+      />
+      <div className="container mx-auto">
+        <hr />
+      </div>
+    </MainLayout>
+  );
+};
 
-   </MainLayout>
-  )
-}
-
-export default HomePage
+export default HomePage;
