3a6dca7ab7f1372bd3ea19932517c698f5562203
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 22:54:05 +0300

    implmented project pagination

--- frontend/src/components/pagination/ProjectPagination.js
+++ frontend/src/components/pagination/ProjectPagination.js
@@ -1,10 +1,18 @@
 import { Pagination } from 'antd';
 import React from 'react'
 
-const ProjectPagination = () => {
+const ProjectPagination = ({ onChange, current, total, pageSize }) => {
   return (
-    <div className='d-flex flex-row justify-content-end mb-4 mt-2'>
-        <Pagination defaultCurrent={1} total={50} />
+    <div className='container mx-auto'>
+        <div className='d-flex flex-row justify-content-end mb-4 mt-2'>
+        <Pagination
+        onChange={onChange}
+        defaultCurrent={1}
+        current={current}
+        total={total}
+        pageSize={pageSize}
+      />
+    </div>
     </div>
    
   )
