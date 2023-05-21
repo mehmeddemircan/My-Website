c7ffbc35925de0c53c39c5bb96eda4bef6d403f0
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 20:46:40 +0300

    on click function added to project cards

--- frontend/src/components/cards/ProjectCard.js
+++ frontend/src/components/cards/ProjectCard.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom'
 
 const ProjectCard = () => {
+
+  const navigate = useNavigate()
   return (
     <Fragment>
-    <div class="my-3 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row hover:shadow-lg hover:translate-y-[-4px]">
+    <div onClick={() => navigate('/project-details')} class="my-3 flex flex-col rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row hover:shadow-lg hover:translate-y-[-4px]">
   <img
     class="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
     src="https://tecdn.b-cdn.net/wp-content/uploads/2020/06/vertical.jpg"
@@ -17,7 +20,7 @@ const ProjectCard = () => {
     </p>
    <div className='d-inline-flex justify-between'>
    <p class="text-xs text-neutral-500 dark:text-neutral-300">Last updated 3 mins ago</p>
-   <a href='/project-details' className='underline'>Details</a>
+   <a href='#' className='underline'>Details</a>
    </div>
   </div>
 </div>
