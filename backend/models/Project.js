e164b1424afeab5b6094eb70f6ac5215580d6fd0
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 16:32:05 +0300

    created project model

---
+++ backend/models/Project.js
@@ -0,0 +1,44 @@
+//replace modelSchema,ModelName with whatever you want
+var mongoose = require('mongoose');
+const {ObjectId} = mongoose.Schema
+var projectSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true 
+    },
+    description: {
+        type: String,
+        required : true 
+    },
+    thumbnailImage: {
+        type: String,
+    },
+    images: [
+        {
+          public_id: {
+            type: String,
+          },
+          url: {
+            type: String,
+          },
+        },
+    ],
+    technologies:[
+        {
+            type : ObjectId,
+            ref : 'TechFeature'
+        }
+    ],
+    webUrl : {
+        type : String
+    },
+    comments : [
+        {
+            type : ObjectId,
+            ref : 'Comment'
+        }
+    ]
+},{timestamps : true});
+
+var Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
