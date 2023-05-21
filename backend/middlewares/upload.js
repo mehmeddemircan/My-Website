fc8c8d3fd6ff5e6db94fbc042e08409038bf1088
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 18:26:10 +0300

    created upload middleware

---
+++ backend/middlewares/upload.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('File type not supported'), false);
+    }
+  }
+});
+
+module.exports = upload;
