f1a5f557f4fb460b067a033ba8e4a16fc3b61d01
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 20:45:29 +0300

    created profile intership feature

---
+++ frontend/src/components/sections/Profile/ProfileIntership.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ProfileIntership = () => {
+  return (
+    <div>
+                <h4 className='my-4'>Staj </h4>
+                <div className='d-inline-flex align-items-center '>
+                    <img className='img-fluid' src='https://theme.zdassets.com/theme_assets/9006636/eafd4582a27da089fa4b9e6c92c37acf22f9f5ae.png' width={80}    />
+                    <div className='ms-3 '>obilet.com</div>
+                    <span className='ms-3'>(  06/2022 - 08/2022  ) <span className='ms-3'>2 ay</span></span>
+                    
+               
+                
+                </div>
+               
+                <div className='mt-3' >
+                    <ul className='ms-4 list-disc'>
+                        <li className='list-item mt-2'>aaa</li>
+                        <li className='list-item mt-2'>aaa</li>
+                        <li className='list-item mt-2'>aaa</li>
+                        <li className='list-item mt-2'>aaa</li>
+                        <li className='list-item mt-2'>aaa</li>
+                        <li className='list-item mt-2'>aaa</li>
+                    </ul>
+                    </div>
+               
+                
+            </div>
+  )
+}
+
+export default ProfileIntership
