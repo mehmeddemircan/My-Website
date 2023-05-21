d2d192a13ba04cb4a1563957d1a46a93e7009e9b
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 18:01:39 +0300

    added routers of created controllers

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { getProjectsWithPage, getProjectDetails, createProject, updateProject, deleteProject } = require('../controllers/projectController');
+const { getProjectsWithPage, getProjectDetails, createProject, updateProject, deleteProject, addTechnologyToProject, removeTechFeatureFromProject } = require('../controllers/projectController');
 
 
 var router = express.Router();
@@ -9,5 +9,9 @@ router.route('/projects/:projectId').get(getProjectDetails)
 router.route('/create-project').post(createProject)
 router.route('/projects/:projectId/update').put(updateProject)
 router.route('/projects/:projectId/delete').delete(deleteProject)
+
+router.route('/projects/:projectId/add-technology').post(addTechnologyToProject);
+router.route('/projects/:projectId/technologies/:techId/remove').delete(removeTechFeatureFromProject)
+// router.delete('/:projectId/remove-tech-feature/:techId', projectController.removeTechFeatureFromProject);
 
 module.exports = router;
