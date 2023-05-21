54ab39ba05e6812110fd9ba6aabadfa0445fdf30
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 21:35:15 +0300

    added these project features components to page

--- frontend/src/pages/ProjectDetailsPage.js
+++ frontend/src/pages/ProjectDetailsPage.js
@@ -3,274 +3,56 @@ import MainLayout from "../layouts/MainLayout";
 import { Image, Space, Tag } from "antd";
 import CommentSections from "../components/sections/CommentSections";
 import CommentModal from "../components/modal/CommentModal";
+import ProjectGallery from "../components/gallery/ProjectGallery";
+import ProjectDescriptionFeature from "../components/sections/Project/ProjectDescriptionFeature";
+import ProjectTechnologies from "../components/sections/Project/ProjectTechnologies";
+import ProjectAccordions from "../components/sections/Project/ProjectAccordions";
 
 const ProjectDetailsPage = () => {
-  const [show, setShow] = useState(false);
-  const [show2, setShow2] = useState(false);
-
-  const [showCommentModal, setShowCommentModal] = useState(false)
+  const [showCommentModal, setShowCommentModal] = useState(false);
 
   const handleShowCommentModal = () => {
-setShowCommentModal(true)
-  }
+    setShowCommentModal(true);
+  };
   const handleCloseCommentModal = () => {
-    setShowCommentModal(false)
-  }
+    setShowCommentModal(false);
+  };
 
   return (
     <MainLayout>
       <div className="container mx-auto">
         {" "}
         <div className="md:flex items-start justify-center py-12 2xl:px-20 md:px-6 px-4">
-          <Image.PreviewGroup
-            preview={{
-              onChange: (current, prev) =>
-                console.log(`current index: ${current}, prev index: ${prev}`),
-            }}
-          >
-            <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-              <div class="grid gap-4">
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div class="grid gap-4">
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div class="grid gap-4">
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div class="grid gap-4">
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <Image
-                    class="h-auto max-w-full rounded-lg"
-                    src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
-                    alt=""
-                  />
-                </div>
-              </div>
-            </div>
-          </Image.PreviewGroup>
+          <div className="col-md-7">
+            <ProjectGallery />
+          </div>
           <div className="xl:w-2/5 md:w-1/2 lg:ml-8 md:ml-6 md:mt-0 mt-6">
-            <div className="border-b border-gray-200 pb-6">
-              <p className="text-sm leading-none text-gray-600">
-                Balenciaga Fall Collection
-              </p>
-              <h1
-                className="
-							lg:text-2xl
-							text-xl
-							font-semibold
-							lg:leading-6
-							leading-7
-							text-gray-800
-							mt-2
-						"
-              >
-                Balenciaga Signature Sweatshirt
-              </h1>
-            </div>
+            <ProjectDescriptionFeature />
 
-            <div>
-              <h5 className="my-3">Teknolojiler</h5>
-              <Space size={[0, 8]} wrap>
-                <Tag color="#f50">#f50</Tag>
-                <Tag color="#2db7f5">#2db7f5</Tag>
-                <Tag color="#87d068">#87d068</Tag>
-                <Tag color="#108ee9">#108ee9</Tag>
-                <Tag color="#f50">#f50</Tag>
-                <Tag color="#2db7f5">#2db7f5</Tag>
-                <Tag color="#87d068">#87d068</Tag>
-                <Tag color="#108ee9">#108ee9</Tag>
-              </Space>
-            </div>
-            <div>
-              <div className="border-t border-b py-4 mt-7 border-gray-200">
-                <div
-                  onClick={() => setShow(!show)}
-                  className="flex justify-between items-center cursor-pointer"
-                >
-                  <p className="text-base leading-4 text-gray-800">
-                    Shipping and returns
-                  </p>
-                  <button
-                    className="
-									cursor-pointer
-									focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400
-									rounded
-								"
-                    aria-label="show or hide"
-                  >
-                    <svg
-                      className={
-                        "transform " + (show ? "rotate-180" : "rotate-0")
-                      }
-                      width="10"
-                      height="6"
-                      viewBox="0 0 10 6"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M9 1L5 5L1 1"
-                        stroke="#4B5563"
-                        strokeWidth="1.25"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </button>
-                </div>
-                <div
-                  className={
-                    "pt-4 text-base leading-normal pr-12 mt-4 text-gray-600 " +
-                    (show ? "block" : "hidden")
-                  }
-                  id="sect"
-                >
-                  You will be responsible for paying for your own shipping costs
-                  for returning your item. Shipping costs are nonrefundable
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="border-b py-4 border-gray-200">
-                <div
-                  onClick={() => setShow2(!show2)}
-                  className="flex justify-between items-center cursor-pointer"
-                >
-                  <p className="text-base leading-4 text-gray-800">
-                    Contact us
-                  </p>
-                  <button
-                    className="
-									cursor-pointer
-									focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400
-									rounded
-								"
-                    aria-label="show or hide"
-                  >
-                    <svg
-                      className={
-                        "transform " + (show2 ? "rotate-180" : "rotate-0")
-                      }
-                      width="10"
-                      height="6"
-                      viewBox="0 0 10 6"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M9 1L5 5L1 1"
-                        stroke="#4B5563"
-                        strokeWidth="1.25"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </button>
-                </div>
-                <div
-                  className={
-                    "pt-4 text-base leading-normal pr-12 mt-4 text-gray-600 " +
-                    (show2 ? "block" : "hidden")
-                  }
-                  id="sect"
-                >
-                  If you have any questions on how to return your item to us,
-                  contact us.
-                </div>
-              </div>
-            </div>
+            <ProjectTechnologies />
+            <ProjectAccordions />
           </div>
         </div>
         <div class="inline-flex items-center justify-center w-full">
-    <hr class="w-100 h-px my-8 bg-gray-400 border-0 dark:bg-gray-700" />
-    <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">Yorumlar</span>
-</div>
+          <hr class="w-100 h-px my-8 bg-gray-400 border-0 dark:bg-gray-700" />
+          <span class="absolute px-3 font-medium text-gray-900 -translate-x-1/2 bg-white left-1/2 dark:text-white dark:bg-gray-900">
+            Yorumlar
+          </span>
+        </div>
         <div className="d-flex flex-row justify-end my-3">
-      
-       <button className="btn btn-dark rounded-pill" onClick={handleShowCommentModal}>Yorum Yap</button>
-                <CommentModal 
-                  showCommentModal={showCommentModal}
-                  handleCloseCommentModal={handleCloseCommentModal}
-                />
+          <button
+            className="btn btn-dark rounded-pill"
+            onClick={handleShowCommentModal}
+          >
+            Yorum Yap
+          </button>
+          <CommentModal
+            showCommentModal={showCommentModal}
+            handleCloseCommentModal={handleCloseCommentModal}
+          />
+        </div>
+        <CommentSections />
       </div>
-      <CommentSections />
-      </div>
-     
     </MainLayout>
   );
 };
