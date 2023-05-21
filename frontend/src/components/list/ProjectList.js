8bf9120b4c2d9feaa0287ef15590462d44e2c836
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-21 22:53:52 +0300

    added loading spinner and listed projects

--- frontend/src/components/list/ProjectList.js
+++ frontend/src/components/list/ProjectList.js
@@ -1,24 +1,29 @@
-import React from 'react'
-import ProjectCard from '../cards/ProjectCard'
-import ProjectPagination from '../pagination/ProjectPagination'
+import React from "react";
+import ProjectCard from "../cards/ProjectCard";
+import ProjectPagination from "../pagination/ProjectPagination";
+import { useSelector } from "react-redux";
+import LoadingSpinner from "../spinner/LoadingSpinner";
 
 const ProjectList = () => {
-  // map ile projeeler gezilecek 
-  return (
-    <div className='container mx-auto'>
-         <h1 className='text-xl my-3 text-center'>Projects</h1>
-         <hr />
-        <div className='d-flex  flex-wrap  justify-content-between'>
-           
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-        </div>
-        <ProjectPagination />
-        <hr />
-    </div>
-  )
-}
+  // map ile projeeler gezilecek
 
-export default ProjectList
+  const getAllProject = useSelector((state) => state.project.getAllProject);
+
+  return (
+    <div className="container mx-auto ">
+      <h1 className="text-xl my-3 text-center">Projects</h1>
+      <hr />
+      {getAllProject.loading ? (
+        <LoadingSpinner />
+      ) : (
+        <div className="d-flex  flex-wrap  justify-content-between">
+          {getAllProject.projects.map((project) => (
+            <ProjectCard key={project._id} project={project} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProjectList;
