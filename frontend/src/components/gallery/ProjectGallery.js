4a9590b3d3a2a61991033e993b33a60533e8f3ce
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-22 16:01:41 +0300

    solved grid problem

--- frontend/src/components/gallery/ProjectGallery.js
+++ frontend/src/components/gallery/ProjectGallery.js
@@ -1,36 +1,36 @@
-import { Image } from 'antd'
-import React, { Fragment } from 'react'
-import { useSelector } from 'react-redux'
+import { Image } from "antd";
+import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
 
 const ProjectGallery = () => {
-
-  const getProjectDetails = useSelector((state) => state.project.getProjectDetails)
+  const getProjectDetails = useSelector(
+    (state) => state.project.getProjectDetails
+  );
 
   return (
     <Fragment>
-         <Image.PreviewGroup
-            preview={{
-              onChange: (current, prev) =>
-                console.log(`current index: ${current}, prev index: ${prev}`),
-            }}
-          >
-            <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-              <div class="grid gap-4">
-                {getProjectDetails.project.images.map((image) => (
-                   <div>
-                   <Image
-                     class="h-auto max-w-full rounded-lg"
-                     src={image.url}
-                     alt=""
-                   />
-                 </div>
-              
-                ))}
+      <Image.PreviewGroup
+        preview={{
+          onChange: (current, prev) =>
+            console.log(`current index: ${current}, prev index: ${prev}`),
+        }}
+      >
+        <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+          {getProjectDetails.project.images.map((image) => (
+            <div class="grid gap-4">
+              <div>
+                <Image
+                  class="h-auto max-w-full rounded-lg"
+                  src={image.url}
+                  alt=""
+                />
               </div>
             </div>
-          </Image.PreviewGroup>
+          ))}
+        </div>
+      </Image.PreviewGroup>
     </Fragment>
-  )
-}
+  );
+};
 
-export default ProjectGallery
+export default ProjectGallery;
