28ba797b0c274f33f858334a3fb88abde560b675
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-22 16:01:11 +0300

    removed add technology and remove routes

--- backend/routes/projectRoutes.js
+++ backend/routes/projectRoutes.js
@@ -10,8 +10,8 @@ router.route('/create-project').post(createProject)
 router.route('/projects/:projectId/update').put(updateProject)
 router.route('/projects/:projectId/delete').delete(deleteProject)
 
-router.route('/projects/:projectId/add-technology').post(addTechnologyToProject);
-router.route('/projects/:projectId/technologies/:techId/remove').delete(removeTechFeatureFromProject)
+// router.route('/projects/:projectId/add-technology').post(addTechnologyToProject);
+// router.route('/projects/:projectId/technologies/:techId/remove').delete(removeTechFeatureFromProject)
 // router.delete('/:projectId/remove-tech-feature/:techId', projectController.removeTechFeatureFromProject);
 
 module.exports = router;
