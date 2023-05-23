a9d71728ce1feeb32325cb200e01be89bcfcf686
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-23 10:37:14 +0300

    added projectId ,userId,parentId fields to comment

--- backend/models/Comment.js
+++ backend/models/Comment.js
@@ -1,11 +1,23 @@
 //replace modelSchema,ModelName with whatever you want
 var mongoose = require('mongoose');
-
+const {ObjectId} = mongoose.Schema
 var commentSchema = new mongoose.Schema({
     text: {
         type: String,
         required : true 
     },
+    projectId : {
+        type: ObjectId, 
+        ref: 'Project' 
+    },
+    userId: {
+        type: ObjectId, 
+        ref: 'User' 
+    },
+    parentCommentId: {
+        type: ObjectId, 
+        ref: 'Comment' 
+    }
    
 },{timestamps: true});
 
