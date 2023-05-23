8b020620be039a3a4ca7137eaa8596c3dd9bee60
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-23 22:36:56 +0300

    created image to not found page

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -6,7 +6,10 @@ const NotFoundPage = () => {
    <MainLayout>
      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
     <div className="text-center">
-      <p className="text-base font-semibold text-indigo-600">404</p>
+  <div className='d-flex justify-center'>
+  <img  width={'50%'} src='https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=826&t=st=1684868724~exp=1684869324~hmac=f76cd3716dd94d47e68bc95c6e9f72f4f384932432dfa311f41a2501b8ae8dad' />
+  </div>
+     
       <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
       <p className="mt-6 text-base leading-7 text-gray-600">Sorry, we couldn’t find the page you’re looking for.</p>
       <div className="mt-10 flex items-center justify-center gap-x-6">
