01191ce5566e3d6d517d39ba9cd4c756ddb60352
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-23 22:36:45 +0300

    created join to form page

---
+++ frontend/src/pages/JoinToTeamFormPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+import JoinToTeamForm from '../components/forms/JoinToTeamForm'
+
+const JoinToTeamFormPage = () => {
+  return (
+    <MainLayout>
+      <JoinToTeamForm />
+    </MainLayout>
+  )
+}
+
+export default JoinToTeamFormPage
