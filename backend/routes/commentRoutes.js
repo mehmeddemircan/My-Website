b201b585c20557add20a8c54b1c86dad616fb1ba
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-23 17:46:03 +0300

    created comment routes for comment

---
+++ backend/routes/commentRoutes.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const { getAllComment, getCommentReplies, deleteComment, createComment, updateComment } = require('../controllers/commentController');
+const { isAuthenticatedUser } = require('../middlewares/auth');
+
+
+var router = express.Router();
+
+router.route('/projects/:projectId/comments').get(getAllComment)
+router.route('/comments/:commentId/replies').get(getCommentReplies)
+
+// kullanici griiş yapmadan calismayacak routerlar
+router.route('/create-comment').post(createComment)
+router.route('/comments/:commentId/update').put(updateComment)
+router.route('/comments/:commentId/delete').delete(deleteComment)
+
+module.exports = router;
