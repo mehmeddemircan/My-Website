386d5c5e32438d49c1d089612d29536755042c56
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-23 22:34:39 +0300

    created success result component

---
+++ frontend/src/components/result/SuccessResult.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Empty } from "antd";
+const SuccessResult = () => {
+  return (
+    <div
+      className="my-4 d-flex flex-row justify-content-center align-items-center"
+    >
+      <Empty
+        description={<h4  className="text-xl">Başarılı Şekilde Formunu aldık , teşekkür ederiz size geri dönüs sağlayacağız</h4>}
+        image="https://img.freepik.com/free-vector/tiny-people-standing-near-big-checkmark-team-male-female-characters-finishing-work-with-list-good-job-sign-flat-vector-illustration-done-job-checklist-time-management-concept_74855-21019.jpg?size=626&ext=jpg&ga=GA1.1.421753172.1680698139&semt=ais"
+        imageStyle={{ width: "100%", height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default SuccessResult;
