aef7ba57a19a165934603f14f7a858425165c92e
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-24 20:34:43 +0300

    create movies page

---
+++ frontend/src/pages/MoviesPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+import EmptyResult from '../components/result/EmptyResult'
+
+const MoviesPage = () => {
+  return (
+   <MainLayout>
+    <EmptyResult />
+   </MainLayout>
+  )
+}
+
+export default MoviesPage
