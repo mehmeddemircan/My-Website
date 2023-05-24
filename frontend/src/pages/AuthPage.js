aa7afa9174556e9b3be893a431634329c40f727c
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-24 12:29:18 +0300

    created auth page

---
+++ frontend/src/pages/AuthPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+import AuthForm from '../components/forms/AuthForm'
+
+const AuthPage = () => {
+  return (
+ 
+        <AuthForm />
+   
+  )
+}
+
+export default AuthPage
