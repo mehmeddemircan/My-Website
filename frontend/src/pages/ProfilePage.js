a14a16fac9ef9001e3e6901e1591c50801d62ff5
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-24 20:34:54 +0300

    created profile page to update profile

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import MainLayout from '../layouts/MainLayout'
-import ProfileSections from '../components/sections/ProfileSections'
 
 const ProfilePage = () => {
   return (
     <MainLayout>
-        <div className='container mx-auto my-5'>
-            <ProfileSections />
+        <div className='container mx-auto'>
+        <h2>Profile Page</h2>
         </div>
     </MainLayout>
   )
