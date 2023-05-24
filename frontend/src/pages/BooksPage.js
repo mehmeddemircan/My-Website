3ed70833b7d477e7681c91ad3a3febd3116ce75e
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-24 20:34:32 +0300

    created books page

---
+++ frontend/src/pages/BooksPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+import EmptyResult from '../components/result/EmptyResult'
+
+const BooksPage = () => {
+  return (
+    <MainLayout>
+        <EmptyResult />
+    </MainLayout>
+  )
+}
+
+export default BooksPage
