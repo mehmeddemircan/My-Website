694c602fed23d12035039e43e2a2c0232bb6242c
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-25 13:04:19 +0300

    added components to profile page

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import MainLayout from '../layouts/MainLayout'
+import ProfileForm from '../components/forms/ProfileForm'
+import UserProfileCard from '../components/cards/UserProfileCard'
 
 const ProfilePage = () => {
   return (
     <MainLayout>
         <div className='container mx-auto'>
-        <h2>Profile Page</h2>
+    
+        <div className='row'>
+        <div className='col-md-4 col-sm-12'>
+        <UserProfileCard />
+      
+        </div>
+        <div className='col-md-8 col-sm-12'>
+        <ProfileForm />
+      
+        </div>
+        </div>
         </div>
     </MainLayout>
   )
