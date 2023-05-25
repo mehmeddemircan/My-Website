37b7997806e9c564a66fff79fd1d0aeb7fd14831
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-25 19:40:04 +0300

    created reply comment modal

---
+++ frontend/src/components/modal/ReplyCommentModal.js
@@ -0,0 +1,29 @@
+import { Modal } from 'antd'
+import React from 'react'
+import CommentForm from '../forms/CommentForm'
+
+const ReplyCommentModal = ({showReplyCommentModal,handleCloseReplyCommentModal}) => {
+  return (
+    <Modal
+      centered
+ 
+      open={showReplyCommentModal}
+      onOk={handleCloseReplyCommentModal}
+      onCancel={handleCloseReplyCommentModal}
+      footer={
+        [
+            <button className="btn btn-dark rounded-pill" onClick={handleCloseReplyCommentModal}>Gönder</button>
+        ]
+      }
+    >
+      <div class="flex justify-between items-center mb-6">
+        <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
+          Yoruma Cevap Ver 
+        </h2>
+      </div>
+        <CommentForm />
+    </Modal>
+  )
+}
+
+export default ReplyCommentModal
