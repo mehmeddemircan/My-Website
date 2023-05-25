94a16fe40b7dcf90a4a3ecc546e48229062dd3f1
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-25 19:41:39 +0300

    created project idea form page

---
+++ frontend/src/pages/ProjectIdeaFormPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+
+const ProjectIdeaFormPage = () => {
+  return (
+   <MainLayout>
+    <div className='container mx-auto'>
+    Proje Fikirinde Bulun
+    </div>
+   </MainLayout>
+  )
+}
+
+export default ProjectIdeaFormPage
