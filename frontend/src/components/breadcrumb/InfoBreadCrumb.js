99ac96942d0229cc3be7334f4e1d835ba0641952
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-25 15:18:23 +0300

    added antd bread crumb as generic

--- frontend/src/components/breadcrumb/InfoBreadCrumb.js
+++ frontend/src/components/breadcrumb/InfoBreadCrumb.js
@@ -1,9 +1,19 @@
+import { Breadcrumb } from 'antd'
 import React from 'react'
 
-const InfoBreadCrumb = () => {
+// hangi sayfada  olduğunu gösteren breadcrumb
+const InfoBreadcrumb = ({separator = '>' , items =[]}) => {
   return (
-    <div>InfoBreadCrumb</div>
+    <div class="container mb-3">
+      
+      <Breadcrumb   separator={separator} >
+    {items.map((item, index) => (
+      <Breadcrumb.Item key={index}>{item.title}</Breadcrumb.Item>
+    ))}
+  </Breadcrumb>
+     
+    </div>
   )
 }
 
-export default InfoBreadCrumb
+export default InfoBreadcrumb
