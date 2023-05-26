d78cf2a6e5fe5c9803db72dcc5a4f087f894107d
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-27 01:20:37 +0300

    removed auth actions from user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 var express = require("express");
 const {
-  login,
-  register,
+
   getAllUser,
   deleteUser,
   getUserComments,
@@ -9,8 +8,7 @@ const {
 
 var router = express.Router();
 
-router.route("/register").post(register);
-router.route("/login").post(login);
+
 
 router.route("/users").get(getAllUser);
 router.route("/users/:userId/delete").delete(deleteUser);
