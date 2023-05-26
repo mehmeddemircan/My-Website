2f6fe7bf2f027a84b52e4d032f884c15597137f3
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-27 01:20:11 +0300

    added get user comments controller removed auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,7 @@
-
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
 const User = require("../models/User");
 const sendToken = require("../utils/sendToken");
-
+const Comment = require("../models/Comment");
 
 // kayıt olma işlemi
 exports.register = catchAsyncErrors(async (req, res, next) => {
@@ -31,94 +30,58 @@ exports.register = catchAsyncErrors(async (req, res, next) => {
     email,
     password,
   });
-  // token oluşturma 
+  // token oluşturma
   sendToken(user, 201, res, "Başarılı şekilde hesap oluşturuldu ");
 });
 
-// giriş yapma controller
-exports.login = catchAsyncErrors(async (req, res, next) => {
-  const { email, password } = req.body;
-
-    // email ve şifreyi null olup olmadıgını kontrol et
-  if (!email || !password) {
-    res.status(400).json({
-      success: false,
-      error: "Lütfen email ve şifre alanlarını doldurunuz ",
+exports.getAllUser = catchAsyncErrors(async (req, res) => {
+  try {
+    const allUser = await User.find();
+    res.status(200).json({
+      success: true,
+      data: allUser,
     });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-
-  // kullanici bulma işlemi 
-  const user = await User.findOne({ email }).select("+password");
-
-  if (!user) {
-    res.status(400).json({
-      success: false,
-      error: "Geçersiz email veya şifre tekrar deneyiniz",
-    });
-  }
-
-  // şifreyi doğru olup olmadığını kontrol etme işlemi 
-  const isPasswordMatched = await user.comparePassword(password);
-
-  if (!isPasswordMatched) {
-    res.status(400).json({
-      success: false,
-      error: "Geçersiz şifre",
-    });
-  }
-  // token oluşturma ve cevap gönderme 
-  sendToken(user, 201, res, "Başarılı Şekilde hesaba girildi ");
 });
 
+exports.deleteUser = catchAsyncErrors(async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.status(200).json({
+      success: true,
+      message: "Başarılı şekilde kullanici silindi ",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-exports.getAllUser = catchAsyncErrors(async(req,res) => {
-    try {
+// update profile
 
+exports.getUserComments = catchAsyncErrors(async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 4;
 
-        const allUser = await User.find()
-        res.status(200).json({
-            success : true ,
-            data : allUser
-        })
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
 
-    } catch (error) {
-        res.status(500).json({error : error.message})
-    }
-})
+    const totalComments = await Comment.find({
+      userId: req.params.userId,
+    }).countDocuments();
 
-exports.deleteUser = catchAsyncErrors(async(req,res) => {
-    try {
-            await User.findByIdAndDelete(req.params.userId)
-            res.status(200).json({
-                success : true ,
-                message :'Başarılı şekilde kullanici silindi '
-            })
-    } catch (error) {
-        res.status(500).json({error : error.message})
-    }
-})
-
-// update profile 
-
-
-exports.getUserComments = catchAsyncErrors(async(req,res) => {
-    try {
-      const page = parseInt(req.query.page) || 1; 
-  const limit = parseInt(req.query.limit) || 4; 
-
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  const totalComments = await Comment.find({userId : req.params.userId}).countDocuments();
-
-  const comments = await Comment.find({userId : req.params.userId}).skip(startIndex).limit(limit);
-  res.status(200).json({
-    totalComments,
-      currentPage : page ,
+    const comments = await Comment.find({ userId: req.params.userId })
+      .skip(startIndex)
+      .limit(limit);
+    res.status(200).json({
+      totalComments,
+      currentPage: page,
       totalPages: Math.ceil(totalComments / limit),
-      results: comments
-  })
-    } catch (error) {
-        res.status(500).json({error : error.message})
-    }
-})
+      results: comments,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
