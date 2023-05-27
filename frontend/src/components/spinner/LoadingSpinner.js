b3e47ef60af65cd6bafda0cef3521afdad4b463b
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-28 00:06:38 +0300

    align center loading spinner

--- frontend/src/components/spinner/LoadingSpinner.js
+++ frontend/src/components/spinner/LoadingSpinner.js
@@ -1,7 +1,7 @@
 import { Space, Spin } from 'antd';
 const LoadingSpinner = () => {
     return (
-        <Space className='d-flex  flex-1  justify-content-center align-items-center'>
+        <Space className='d-flex  flex-1  justify-content-center align-items-center' style={{height :'35vh'}}>
             <Spin size='large' />
         </Space>
     )
