05678f1c87bf3c5b5c2e53dba47e42be45e277e7
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-28 00:06:48 +0300

    created generic tabs for my forms page

---
+++ frontend/src/components/tabs/GenericTabs.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+
+const GenericTabs = ({ tabs }) => {
+  return (
+    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+    <Tabs defaultActiveKey={tabs[0].key} centered>
+      {tabs.map((tab) => (
+        <TabPane key={tab.key} tab={<span>{tab.title}</span>}>
+          {tab.component}
+        </TabPane>
+      ))}
+    </Tabs>
+  </div>
+  );
+};
+
+export default GenericTabs;
