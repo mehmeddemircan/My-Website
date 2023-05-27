c0868b70fcb59c5e2585b10a442f84858c55b51b
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-28 00:06:24 +0300

    added buttons to success result

--- frontend/src/components/result/SuccessResult.js
+++ frontend/src/components/result/SuccessResult.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Empty } from "antd";
-const SuccessResult = () => {
+const SuccessResult = ({buttonComp}) => {
   return (
     <div
       className="my-4 d-flex flex-row justify-content-center align-items-center"
     >
       <Empty
-        description={<h4  className="text-xl">Başarılı Şekilde Formunu aldık , teşekkür ederiz size geri dönüs sağlayacağız</h4>}
+        description={<Fragment><h4  className="text-xl mb-4">Başarılı Şekilde Formunu aldık , teşekkür ederiz size geri dönüs sağlayacağız</h4><div>{buttonComp}</div></Fragment>}
         image="https://img.freepik.com/free-vector/tiny-people-standing-near-big-checkmark-team-male-female-characters-finishing-work-with-list-good-job-sign-flat-vector-illustration-done-job-checklist-time-management-concept_74855-21019.jpg?size=626&ext=jpg&ga=GA1.1.421753172.1680698139&semt=ais"
         imageStyle={{ width: "100%", height: "100%" }}
       />
+      
     </div>
   );
 };
