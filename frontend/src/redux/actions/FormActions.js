bd4d739b0936d99b80e9c7fc6f5b6b6e468a674e
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-28 00:07:52 +0300

    added send, delete update give job form functions

--- frontend/src/redux/actions/FormActions.js
+++ frontend/src/redux/actions/FormActions.js
@@ -1,6 +1,6 @@
 
 import axios from "axios";
-import { GET_CITIES_FAIL, GET_CITIES_REQUEST, GET_CITIES_SUCCESS } from "../constants/FormConstants";
+import { DELETE_MY_GIVE_JOB_FORMS_FAIL, DELETE_MY_GIVE_JOB_FORMS_REQUEST, DELETE_MY_GIVE_JOB_FORMS_SUCCESS, GET_CITIES_FAIL, GET_CITIES_REQUEST, GET_CITIES_SUCCESS, GET_MY_GIVE_JOB_FORMS_FAIL, GET_MY_GIVE_JOB_FORMS_REQUEST, GET_MY_GIVE_JOB_FORMS_SUCCESS, SEND_GIVE_JOB_FORM_FAIL, SEND_GIVE_JOB_FORM_REQUEST, SEND_GIVE_JOB_FORM_SUCCESS, UPDATE_MY_GIVE_JOB_FORMS_FAIL, UPDATE_MY_GIVE_JOB_FORMS_REQUEST, UPDATE_MY_GIVE_JOB_FORMS_SUCCESS } from "../constants/FormConstants";
 
 
 export const GetCities = () => async (dispatch) => {
@@ -24,3 +24,97 @@ export const GetCities = () => async (dispatch) => {
       });
     }
   };
+
+
+  export const SendGiveJobForm = (giveJobForm) => async (dispatch) => {
+    try {
+      dispatch({
+        type: SEND_GIVE_JOB_FORM_REQUEST,
+      });
+  
+      const { data } = await axios.post(
+        `http://localhost:5000/api/giveJobForms/submit`,giveJobForm
+      );
+  
+      dispatch({
+        type: SEND_GIVE_JOB_FORM_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: SEND_GIVE_JOB_FORM_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+  export const GetUserGiveJobForms = (userId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: GET_MY_GIVE_JOB_FORMS_REQUEST,
+      });
+  
+      const { data } = await axios.get(
+        `http://localhost:5000/api/users/${userId}/giveJobForms`
+      );
+  
+      dispatch({
+        type: GET_MY_GIVE_JOB_FORMS_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: GET_MY_GIVE_JOB_FORMS_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+  export const DeleteUserGiveJobForms = (formId) => async (dispatch) => {
+    try {
+      dispatch({
+        type: DELETE_MY_GIVE_JOB_FORMS_REQUEST,
+      });
+  
+      const { data } = await axios.delete(
+        `http://localhost:5000/api/giveJobForms/${formId}/delete`
+      );
+  
+      dispatch({
+        type: DELETE_MY_GIVE_JOB_FORMS_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: DELETE_MY_GIVE_JOB_FORMS_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+
+  export const UpdateUserGiveJobForms = (formId,form) => async (dispatch) => {
+    try {
+      dispatch({
+        type: UPDATE_MY_GIVE_JOB_FORMS_REQUEST,
+      });
+  
+      const { data } = await axios.put(
+        `http://localhost:5000/api/giveJobForms/${formId}/update`,form
+      );
+  
+      dispatch({
+        type: UPDATE_MY_GIVE_JOB_FORMS_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: UPDATE_MY_GIVE_JOB_FORMS_FAIL,
+        error: error.response,
+      });
+    }
+  };
+
+  
+
+  
--- frontend/src/redux/reducers/FormReducer.js
+++ frontend/src/redux/reducers/FormReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { GET_CITIES_FAIL, GET_CITIES_REQUEST, GET_CITIES_SUCCESS } from "../constants/FormConstants";
+import { DELETE_MY_GIVE_JOB_FORMS_FAIL, DELETE_MY_GIVE_JOB_FORMS_REQUEST, DELETE_MY_GIVE_JOB_FORMS_RESET, DELETE_MY_GIVE_JOB_FORMS_SUCCESS, GET_CITIES_FAIL, GET_CITIES_REQUEST, GET_CITIES_SUCCESS, GET_MY_GIVE_JOB_FORMS_FAIL, GET_MY_GIVE_JOB_FORMS_REQUEST, GET_MY_GIVE_JOB_FORMS_SUCCESS, SEND_GIVE_JOB_FORM_FAIL, SEND_GIVE_JOB_FORM_REQUEST, SEND_GIVE_JOB_FORM_RESET, SEND_GIVE_JOB_FORM_SUCCESS, UPDATE_MY_GIVE_JOB_FORMS_FAIL, UPDATE_MY_GIVE_JOB_FORMS_REQUEST, UPDATE_MY_GIVE_JOB_FORMS_RESET, UPDATE_MY_GIVE_JOB_FORMS_SUCCESS } from "../constants/FormConstants";
 
 
 const GET_CITIES_INITIAL_STATE = {
@@ -34,9 +34,132 @@ const GET_CITIES_INITIAL_STATE = {
     }
   };
 
+ 
+  export const sendGiveJobFormReducer = (
+    state = {message : ""},
+    action
+  ) => {
+    switch (action.type) {
+      case SEND_GIVE_JOB_FORM_REQUEST:
+        return { ...state, loading: true };
+  
+      case SEND_GIVE_JOB_FORM_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          success: true,
+          message : action.payload.message
+        };
+  
+      case SEND_GIVE_JOB_FORM_FAIL:
+        return {
+          ...state,
+          loading: false,
+          success: false,
+          error: action.payload,
+        };
+      case SEND_GIVE_JOB_FORM_RESET : 
+        return {
+            ...state,
+            success : false 
+        }
+      default:
+        return state;
+    }
+  };
+
+  export const getUserGiveJobFormsReducer = (
+    state = { giveJobForms : []},
+    action
+  ) => {
+    switch (action.type) {
+      case GET_MY_GIVE_JOB_FORMS_REQUEST:
+        return { ...state, loading: true };
+  
+      case GET_MY_GIVE_JOB_FORMS_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          success: true,
+             giveJobForms : action.payload.results
+        };
+  
+      case GET_MY_GIVE_JOB_FORMS_FAIL:
+        return {
+          ...state,
+          loading: false,
+          success: false,
+          error: action.payload,
+        };
+      
+      default:
+        return state;
+    }
+  };
+
+  export const deleteUpdateUserGiveJobFormsReducer = (
+    state = { message : ""},
+    action
+  ) => {
+    switch (action.type) {
+      case DELETE_MY_GIVE_JOB_FORMS_REQUEST:
+      case UPDATE_MY_GIVE_JOB_FORMS_REQUEST : 
+        return { ...state, loading: true };
+  
+      case DELETE_MY_GIVE_JOB_FORMS_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          isDeleted: true,
+            message : action.payload.message
+        };
+  
+        case UPDATE_MY_GIVE_JOB_FORMS_SUCCESS:
+            return {
+              ...state,
+              loading: false,
+              isUpdated: true,
+                message : action.payload.message
+            };
+      
+      case DELETE_MY_GIVE_JOB_FORMS_FAIL:
+        return {
+          ...state,
+          loading: false,
+          isDeleted: false,
+          error: action.payload,
+        };
+        case UPDATE_MY_GIVE_JOB_FORMS_FAIL:
+            return {
+              ...state,
+              loading: false,
+              isUpdated: false,
+              error: action.payload,
+            };
+      case DELETE_MY_GIVE_JOB_FORMS_RESET : 
+        return {
+            ...state,
+            isDeleted : false 
+        }
+        case UPDATE_MY_GIVE_JOB_FORMS_RESET : 
+        return {
+            ...state,
+            isUpdated : false 
+        }
+      
+      
+      default:
+        return state;
+    }
+  };
+
+
 
   const formReducer = combineReducers({
-    getCities : getCitiesReducer
+    getCities : getCitiesReducer,
+    sendGiveJobForm : sendGiveJobFormReducer,
+    getUserGiveJobForms : getUserGiveJobFormsReducer,
+    deleteUpdateUserGiveJobForms : deleteUpdateUserGiveJobFormsReducer
   })
 
   export default formReducer
