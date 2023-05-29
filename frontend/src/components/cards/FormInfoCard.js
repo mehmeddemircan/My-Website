fa8312b9d49a79f2d090e2106489997f5d331485
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-29 10:51:50 +0300

    isTeamForm isGiveJobForm props added form card

--- frontend/src/components/cards/FormInfoCard.js
+++ frontend/src/components/cards/FormInfoCard.js
@@ -1,10 +1,11 @@
-import { Descriptions, List, Tooltip } from "antd";
+import { Descriptions, List, Tag, Tooltip } from "antd";
 import React, { Fragment, useEffect, useState } from "react";
 import UpdateFormModal from "../modal/form/UpdateFormModal";
 import { useDispatch, useSelector } from "react-redux";
 import GiveJobForm from "../forms/GiveJobForm";
+import JoinToTeamForm from "../forms/JoinToTeamForm";
 
-const FormInfoCard = ({ handleDeleteGiveJobForm, form }) => {
+const FormInfoCard = ({ isGiveJobForm,isTeamForm,handleDeleteTeamForm,handleDeleteGiveJobForm, form }) => {
 
 
   const [showUpdateFormModal, setShowUpdateFormModal] = useState(false);
@@ -25,6 +26,9 @@ const FormInfoCard = ({ handleDeleteGiveJobForm, form }) => {
   const [companyName, setCompanyName] = useState(form.companyName);
   const [city, setCity] = useState(form.city);
   const [additionalInfo, setAdditionalInfo] = useState(form.additionalInfo);
+// team Forms 
+const [staticDepartments, setStaticDepartments] = useState(["Backend","Frontend","UI/UX Designer","Mobile Developer","Database Manager","Network Manager"])
+const [departments, setDepartments] = useState(form.departments)
 
   useEffect(() => {
     if (auth.authenticate) {
@@ -41,12 +45,37 @@ const FormInfoCard = ({ handleDeleteGiveJobForm, form }) => {
               <a>User Info</a>{" "}
               <div>
                 <button
-                  className="btn btn-outline-success rounded-pill mx-1"
+                  className="btn btn-outline-primary rounded-pill mx-1"
                   onClick={handleShowUpdateFormModal}
                 >
                   Düzenle
                 </button>
-                <UpdateFormModal
+                {isTeamForm && (
+                   <UpdateFormModal
+                   showUpdateFormModal={showUpdateFormModal}
+                   handleCloseUpdateFormModal={handleCloseUpdateFormModal}
+                 >
+                      <JoinToTeamForm 
+        isEditForm={true}
+        staticDepartments={staticDepartments}
+        firstname={firstname}
+        setFirstname={setFirstname}
+        lastname={lastname}
+        setLastname={setLastname}
+        email={email}
+        setEmail={setEmail}
+        phoneNumber={phoneNumber}
+        setPhoneNumber={setPhoneNumber}
+        departments={departments}
+        setDepartments={setDepartments}
+        additionalInfo={additionalInfo}
+        setAdditionalInfo={setAdditionalInfo}
+        // handleSendForm={handleSendForm}
+      />
+                 </UpdateFormModal>
+                )}
+                {isGiveJobForm && (
+                  <UpdateFormModal
                   showUpdateFormModal={showUpdateFormModal}
                   handleCloseUpdateFormModal={handleCloseUpdateFormModal}
                 >
@@ -70,11 +99,13 @@ const FormInfoCard = ({ handleDeleteGiveJobForm, form }) => {
                     //   handleSendForm={handleSendForm}
                   />
                 </UpdateFormModal>
+                )}
+                
 
                 <Tooltip title="Delete">
                   <button
                     className="btn btn-light"
-                    onClick={() => handleDeleteGiveJobForm(form._id)}
+                    onClick={() => isGiveJobForm ?  handleDeleteGiveJobForm(form._id) : isTeamForm ? handleDeleteTeamForm(form._id) : null}
                   >
                     <i class="fa-solid fa-trash"></i>
                   </button>
@@ -83,9 +114,20 @@ const FormInfoCard = ({ handleDeleteGiveJobForm, form }) => {
             </div>
           }
         >
-          <Descriptions.Item label="Name"></Descriptions.Item>
-          <Descriptions.Item label="Telephone"></Descriptions.Item>
-          <Descriptions.Item label="Email"></Descriptions.Item>
+          <Descriptions.Item label="İsim">{form.firstname}</Descriptions.Item>
+          <Descriptions.Item label="Soyisim">{form.lastname}</Descriptions.Item>
+          <Descriptions.Item label="Telephone">{form?.phoneNumber}</Descriptions.Item>
+          <Descriptions.Item label="Email">{form.email}</Descriptions.Item>
+          {isGiveJobForm  &&   (
+            <>
+               <Descriptions.Item label="Email">{form.companyName}</Descriptions.Item>
+            <Descriptions.Item label="Email">{form?.city}</Descriptions.Item>
+            </>
+         
+          ) }
+          {
+            isTeamForm  &&  <Descriptions.Item label="Calışmak İstediğiniz bölümler">{form.departments.map((department) => <Tag color="#108ee9">{department}</Tag>)}</Descriptions.Item>
+          }
         </Descriptions>
       </List.Item>
     </Fragment>
