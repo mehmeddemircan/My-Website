c812e8fdda97c412f7a5f20b6e61d5b5d88eb982
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-29 10:52:59 +0300

    jon to team form value onchange added

--- frontend/src/components/forms/JoinToTeamForm.js
+++ frontend/src/components/forms/JoinToTeamForm.js
@@ -3,34 +3,27 @@ import React, { Fragment , useState } from 'react'
 
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Switch } from '@headlessui/react'
-
+import MultipleSelect from '../select/MultipleSelect'
+import { Select } from 'antd'
+import FormButton from '../button/FormButton'
+const {Option} = Select
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
   }
-const JoinToTeamForm = () => {
+const JoinToTeamForm = ({isEditForm,handleSendForm,staticDepartments,firstname,setFirstname,lastname,setLastname,email,setEmail,departments,setDepartments,additionalInfo,setAdditionalInfo,phoneNumber,setPhoneNumber}) => {
     const [agreed, setAgreed] = useState(false)
+
+    
+  
+      const handleSelectDepartment = (values) => {
+        setDepartments(values)
+      }
+
   return (
     <Fragment>
-         <div className="bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div
-        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">
-          Aute magna irure deserunt veniam aliqua magna enim voluptate.
-        </p>
-      </div>
-      <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
+         <div className="bg-white px-6 py-2 sm:py-32 lg:px-8">
+     
+      <form action="#" method="POST" className="mx-auto mt-3 max-w-xl sm:mt-20">
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
             <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
@@ -38,6 +31,8 @@ const JoinToTeamForm = () => {
             </label>
             <div className="mt-2.5">
               <input
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
                 type="text"
                 name="first-name"
                 id="first-name"
@@ -52,6 +47,8 @@ const JoinToTeamForm = () => {
             </label>
             <div className="mt-2.5">
               <input
+                    value={lastname}
+                    onChange={(e) => setLastname(e.target.value)}
                 type="text"
                 name="last-name"
                 id="last-name"
@@ -60,26 +57,15 @@ const JoinToTeamForm = () => {
               />
             </div>
           </div>
-          <div className="sm:col-span-2">
-            <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
-              Company
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="company"
-                id="company"
-                autoComplete="organization"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+        
           <div className="sm:col-span-2">
             <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
               Email
             </label>
             <div className="mt-2.5">
               <input
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
                 type="email"
                 name="email"
                 id="email"
@@ -102,9 +88,8 @@ const JoinToTeamForm = () => {
                   name="country"
                   className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                 >
-                  <option>US</option>
-                  <option>CA</option>
-                  <option>EU</option>
+                  <option>+90</option>
+             
                 </select>
                 <ChevronDownIcon
                   className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
@@ -112,6 +97,8 @@ const JoinToTeamForm = () => {
                 />
               </div>
               <input
+                      value={phoneNumber}
+                      onChange={(e) => setPhoneNumber(e.target.value)}
                 type="tel"
                 name="phone-number"
                 id="phone-number"
@@ -121,11 +108,25 @@ const JoinToTeamForm = () => {
             </div>
           </div>
           <div className="sm:col-span-2">
+            <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
+              Çalışmak İstediğiniz Bölümler 
+            </label>
+            <div className="mt-2.5">
+             <MultipleSelect value={departments} onChange={handleSelectDepartment}>
+              {staticDepartments.map((item) => (
+                <Option value={item}>{item}</Option>
+              ))}
+             </MultipleSelect>
+            </div>
+          </div>
+          <div className="sm:col-span-2">
             <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
               Message
             </label>
             <div className="mt-2.5">
               <textarea
+                value={additionalInfo}
+                onChange={(e) => setAdditionalInfo(e.target.value)}
                 name="message"
                 id="message"
                 rows={4}
@@ -164,12 +165,7 @@ const JoinToTeamForm = () => {
           </Switch.Group>
         </div>
         <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Let's talk
-          </button>
+          <FormButton title={isEditForm ? "Güncelle" : "Gönder"} onClick={handleSendForm} />
         </div>
       </form>
     </div>
