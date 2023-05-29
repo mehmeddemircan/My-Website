69d9ac76870c296e778b42c0b5d2692a9d7d5c9f
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-29 10:53:12 +0300

    prettier generic tabs

--- frontend/src/components/tabs/GenericTabs.js
+++ frontend/src/components/tabs/GenericTabs.js
@@ -4,9 +4,12 @@ import { Tabs } from 'antd';
 const { TabPane } = Tabs;
 
 const GenericTabs = ({ tabs }) => {
+
+    
+
   return (
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-    <Tabs defaultActiveKey={tabs[0].key} centered>
+    <Tabs  defaultActiveKey={tabs[0].key} centered>
       {tabs.map((tab) => (
         <TabPane key={tab.key} tab={<span>{tab.title}</span>}>
           {tab.component}
