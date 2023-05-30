6d4725b75a8365ae01e769b79352c45b6c9793f5
mehmet demircan <mehmeddemircan@gmail.com>
2023-05-30 21:33:08 +0300

    created project idea form design

---
+++ frontend/src/components/forms/ProjectIdeaForm.js
@@ -0,0 +1,162 @@
+
+import React, { Fragment , useState } from 'react'
+
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Switch } from '@headlessui/react'
+import { useSelector } from 'react-redux'
+import FormButton from '../button/FormButton'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+const ProjectIdeaForm = ({isEditForm,handleSendForm,firstname,setFirstname,lastname,setLastname,email,setEmail,phoneNumber,setPhoneNumber,projectIdea,setProjectIdea}) => {
+    const [agreed, setAgreed] = useState("")
+
+
+  return (
+    <Fragment>
+    <div className="bg-white px-6 py-3 sm:py-32 lg:px-8">
+    
+    
+    <form action="#" method="POST" className="mx-auto mt-3 max-w-xl sm:mt-20">
+      <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+        <div>
+          <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+            First name
+          </label>
+          <div className="mt-2.5">
+            <input
+              placeholder='isim yaziniz '
+              type="text"
+              name="first-name"
+              id="first-name"
+              autoComplete="given-name"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={firstname}
+              onChange={(e) => setFirstname(e.target.value)}
+            
+            />
+          </div>
+        </div>
+        <div>
+          <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+            Last name
+          </label>
+          <div className="mt-2.5">
+            <input
+              placeholder='soyisim giriniz '
+              type="text"
+              name="last-name"
+              id="last-name"
+              autoComplete="family-name"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={lastname}
+              onChange={(e) => setLastname(e.target.value)}
+           />
+          </div>
+        </div>
+       
+        <div className="sm:col-span-2">
+          <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+            Email
+          </label>
+          <div className="mt-2.5">
+            <input
+              placeholder='email giriniz '
+              type="email"
+              name="email"
+              id="email"
+              autoComplete="email"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+            Phone number
+          </label>
+          <div className="relative mt-2.5">
+            <div className="absolute inset-y-0 left-0 flex items-center">
+              <label htmlFor="country" className="sr-only">
+                Country
+              </label>
+              <select
+                id="country"
+                name="country"
+                className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-1 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              >
+                <option>+90</option>
+              
+              </select>
+             
+            </div>
+            <input
+              placeholder='5xx-xxx-xx-xx'
+              type="tel"
+              name="phone-number"
+              id="phone-number"
+              autoComplete="tel"
+              className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+            Message
+          </label>
+          <div className="mt-2.5">
+            <textarea
+              placeholder='ek bilgiler girebilirsiniz '
+              name="message"
+              id="message"
+              rows={6}
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+         
+              value={projectIdea}
+              onChange={(e) => setProjectIdea(e.target.value)}
+            />
+          </div>
+        </div>
+        <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+          <div className="flex h-6 items-center">
+            <Switch
+              checked={agreed}
+              onChange={setAgreed}
+              className={classNames(
+                agreed ? 'bg-indigo-600' : 'bg-gray-200',
+                'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+              )}
+            >
+              <span className="sr-only">Agree to policies</span>
+              <span
+                aria-hidden="true"
+                className={classNames(
+                  agreed ? 'translate-x-3.5' : 'translate-x-0',
+                  'h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out'
+                )}
+              />
+            </Switch>
+          </div>
+          <Switch.Label className="text-sm leading-6 text-gray-600">
+            By selecting this, you agree to our{' '}
+            <a href="#" className="font-semibold text-indigo-600">
+              privacy&nbsp;policy
+            </a>
+            .
+          </Switch.Label>
+        </Switch.Group>
+      </div>
+      <div className="mt-10">
+        <FormButton title={isEditForm ? "Güncelle" : "Gönder"} onClick={handleSendForm} />
+      </div>
+    </form>
+  </div>
+  </Fragment>
+  )
+}
+
+export default ProjectIdeaForm
