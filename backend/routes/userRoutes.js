42dfd7ad256b09b99046cc189a226f0a46717d51
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-05 00:11:37 +0300

    added routes of update profile , get profile controllers

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,6 +4,8 @@ const {
   getAllUser,
   deleteUser,
   getUserComments,
+  getUserProfile,
+  updateUserProfile,
 } = require("../controllers/userController");
 
 var router = express.Router();
@@ -14,5 +16,10 @@ router.route("/users").get(getAllUser);
 router.route("/users/:userId/delete").delete(deleteUser);
 
 router.route('/users/:userId/comments').get(getUserComments)
+
+router.route("/profile/me").get(getUserProfile);
+router
+  .route("/profile/update")
+  .put(updateUserProfile);
 
 module.exports = router;
