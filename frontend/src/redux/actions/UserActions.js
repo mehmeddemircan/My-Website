ac2b1e43f70bb92f094e38e71109a2b7c97d8659
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-06 18:47:35 +0300

    created user get profile and update profile redux

--- frontend/src/redux/actions/UserActions.js
+++ frontend/src/redux/actions/UserActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_USER_COMMENTS_FAIL, GET_USER_COMMENTS_REQUEST, GET_USER_COMMENTS_SUCCESS } from "../constants/UserConstants";
+import { GET_PROFILE_FAIL, GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, GET_USER_COMMENTS_FAIL, GET_USER_COMMENTS_REQUEST, GET_USER_COMMENTS_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_SUCCESS } from "../constants/UserConstants";
 
 export const GetUserComments = (userId,limit,currentPage) => async (dispatch) => {
     try {
@@ -22,3 +22,67 @@ export const GetUserComments = (userId,limit,currentPage) => async (dispatch) =>
       });
     }
   };
+
+// profil bilgilerini getir
+export const GetProfile = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_PROFILE_REQUEST,
+    });
+    const token = localStorage.getItem('token');
+  
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+    };
+
+    const { data } = await axios.get(
+      `http://localhost:5000/api/profile/me`,config
+    );
+
+    dispatch({
+      type: GET_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_PROFILE_FAIL,
+      error: error.response,
+    });
+  }
+};
+  
+// profili güncelle
+export const UpdateProfile = (user) => async (dispatch) => {
+  try {
+    dispatch({
+      type: UPDATE_PROFILE_REQUEST,
+    });
+    const token = localStorage.getItem('token');
+  
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`
+      },
+    };
+
+    const { data } = await axios.put(
+      `http://localhost:5000/api/profile/update`,user,config
+    );
+    localStorage.setItem("user", JSON.stringify(data.user))
+    localStorage.setItem("token",data.token)
+    
+    dispatch({
+      type: UPDATE_PROFILE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: UPDATE_PROFILE_FAIL,
+      error: error.response,
+    });
+  }
+};
--- frontend/src/redux/reducers/UserReducer.js
+++ frontend/src/redux/reducers/UserReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import { GET_USER_COMMENTS_FAIL, GET_USER_COMMENTS_REQUEST, GET_USER_COMMENTS_SUCCESS } from "../constants/UserConstants";
+import { GET_PROFILE_FAIL, GET_PROFILE_REQUEST, GET_PROFILE_SUCCESS, GET_USER_COMMENTS_FAIL, GET_USER_COMMENTS_REQUEST, GET_USER_COMMENTS_SUCCESS, UPDATE_PROFILE_FAIL, UPDATE_PROFILE_REQUEST, UPDATE_PROFILE_RESET, UPDATE_PROFILE_SUCCESS } from "../constants/UserConstants";
 
 const GET_USER_COMMENTS_INITIAL_STATE = {
     totalComments : 0,
@@ -35,8 +35,64 @@ const GET_USER_COMMENTS_INITIAL_STATE = {
     }
   };
 
+
+  export const getProfileReducer = (
+    state = {user: {}},
+    action
+  ) => {
+    switch (action.type) {
+      case GET_PROFILE_REQUEST:
+        return { ...state, loading: true };
+  
+      case GET_PROFILE_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          success: true,
+          
+          user: action.payload.user,
+        };
+  
+      case GET_PROFILE_FAIL:
+        return {
+          ...state,
+          loading: false,
+          success: false,
+          error: action.payload,
+        };
+      default:
+        return state;
+    }
+  };
+  export const updateProfileReducer = (state = { token : "", user : {} , message : "" }, action) => {
+    switch (action.type) {
+      case UPDATE_PROFILE_REQUEST:
+        return { ...state, loading: true };
+      case UPDATE_PROFILE_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          success: true,
+          token : action.payload.token,
+          user: action.payload.user,
+          message : action.payload.message
+        };
+  
+      case UPDATE_PROFILE_FAIL:
+        return { ...state, loading: false, error: action.payload };
+  
+        case UPDATE_PROFILE_RESET:
+          return { ...state,success: false, token : "" , user : {} };
+      default:
+        return state;
+    }
+  };
+  
+
   const userReducer = combineReducers({
-        getUserComments : getUserCommentsReducer
+        getUserComments : getUserCommentsReducer,
+        getProfile : getProfileReducer,
+        updateProfile : updateProfileReducer
   })
 
   export default userReducer;
